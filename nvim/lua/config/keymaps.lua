cf20645f065ab430106884e6f6b9008b1943f36f
Kirill Voloshin <tehpolecat@gmail.com>
2023-10-10 22:18:47 +0800

    convenience nvim keybinds

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,3 +1,8 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+local map = vim.keymap.set
+map("n", "<C-d>", "<C-d>zz", { remap = true })
+map("n", "<C-d>", "<C-d>zz", { remap = true })
+map("n", "n", "nzzzv", { remap = true })
+map("n", "N", "Nzzzv", { remap = true })
