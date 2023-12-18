31fdf574b651497c91e412e58fee86ff246edb41
Kirill Voloshin <kirill.voloshin@teampicnic.com>
2023-12-18 13:35:24 +0100

    recognize pod files as ruby files!

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,3 +1,9 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+local auto_command_on = vim.api.nvim_create_autocmd
+
+auto_command_on({ "BufRead", "BufNewFile" }, {
+  pattern = { "*.podspec", "Podfile" },
+  command = "set filetype=ruby",
+})
