3985f8862d32907530ffd19be1614597a147a97c
Kirill Voloshin <kirill.voloshin@teampicnic.com>
2024-03-21 15:30:09 +0100

    disable octo

--- nvim/lua/plugins/octo.lua
+++ nvim/lua/plugins/octo.lua
@@ -1,13 +1,13 @@
-return {
-  {
-    "pwntester/octo.nvim",
-    requires = {
-      "nvim-lua/plenary.nvim",
-      "nvim-telescope/telescope.nvim",
-      "nvim-tree/nvim-web-devicons",
-    },
-    config = function()
-      require("octo").setup()
-    end,
-  },
-}
+-- return {
+--   {
+--     "pwntester/octo.nvim",
+--     requires = {
+--       "nvim-lua/plenary.nvim",
+--       "nvim-telescope/telescope.nvim",
+--       "nvim-tree/nvim-web-devicons",
+--     },
+--     config = function()
+--       require("octo").setup()
+--     end,
+--   },
+-- }
