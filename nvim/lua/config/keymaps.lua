1324dd54f8f6f997f69c1f7762a2335545b99ba0
Kirill Voloshin <tehpolecat@gmail.com>
2024-09-25 22:53:47 +0200

    integrate obsidian

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -6,3 +6,48 @@ map("n", "<C-d>", "<C-d>zz", { remap = true })
 map("n", "<C-u>", "<C-u>zz", { remap = true })
 map("n", "n", "nzzzv", { remap = true })
 map("n", "N", "Nzzzv", { remap = true })
+
+-- creates a note from the template
+map("n", "<leader>on", ":ObsidianTemplate note<cr> :lua vim.cmd([[1,/^\\S/s/^\\n\\{1,}//]])<cr>")
+-- removes timestamps from line and converts underscores to spaces, just a utility for obsidian note titles
+map("n", "<leader>of", ':s/\\(# \\)[^_]*_/\\1/e | s/-/ /ge | let @/ = ""<cr>')
+-- review workflow
+map("n", "<leader>ok", function()
+  local file = vim.fn.expand("%:p")
+  if file == "" then
+    print("No file to move.")
+    return
+  end
+
+  local target_dir = "~/obsidian/personal-vault/zk/"
+  -- Expand tilde to the home directory
+  target_dir = vim.fn.expand(target_dir)
+
+  -- Extract the file name to append to the target directory
+  local filename = vim.fn.fnamemodify(file, ":t")
+  local target_path = target_dir .. filename
+
+  -- Try to move the file
+  local ok, err = pcall(vim.loop.fs_rename, file, target_path)
+  if ok then
+    vim.cmd("bd!")
+    print("File moved to " .. target_path)
+  else
+    print("Error moving file: " .. err)
+  end
+end, { desc = "Accept note" })
+map("n", "<leader>odd", function()
+  local file = vim.fn.expand("%:p")
+  if file == "" then
+    print("No file to delete.")
+    return
+  end
+
+  local ok, err = pcall(vim.fn.delete, file)
+  if ok then
+    vim.cmd("bd!")
+    print("File deleted successfully.")
+  else
+    print("Error deleting file: " .. err)
+  end
+end, { desc = "Delete note" })
--- nvim/lua/plugins/obsidian.lua
+++ nvim/lua/plugins/obsidian.lua
@@ -3,19 +3,9 @@ return {
   version = "*", -- recommended, use latest release instead of latest commit
   lazy = true,
   ft = "markdown",
-  -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
-  -- event = {
-  --   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
-  --   -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/*.md"
-  --   -- refer to `:h file-pattern` for more examples
-  --   "BufReadPre path/to/my-vault/*.md",
-  --   "BufNewFile path/to/my-vault/*.md",
-  -- },
   dependencies = {
     -- Required.
     "nvim-lua/plenary.nvim",
-
-    -- see below for full list of optional dependencies 👇
   },
   opts = {
     workspaces = {
@@ -23,6 +13,13 @@ return {
         name = "personal",
         path = "~/obsidian/personal-vault",
       },
+    },
+
+    disable_frontmatter = true,
+    templates = {
+      subdir = "templates",
+      date_format = "%Y-%m-%d",
+      time_format = "%H:%M:%S",
     },
 
     mappings = {
@@ -33,7 +30,5 @@ return {
         opts = { noremap = false, expr = true, buffer = true },
       },
     },
-
-    -- see below for full list of options 👇
   },
 }
