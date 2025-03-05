1dae5d8749c866779e831db25d0d6bc2a633df47
Kirill Voloshin <kirill.voloshin@teampicnic.com>
2025-03-05 14:21:16 +0100

    add typescript quickfix support to nvim

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -1,3 +1,10 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+local augroup = vim.api.nvim_create_augroup("typescript quickfix", { clear = true })
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "typescript,typescriptreact",
+  group = augroup,
+  command = "compiler tsc | setlocal makeprg=npx\\ tsc",
+})
