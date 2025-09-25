ffa2b8156b3ca43e2ad9434ec93e31f3c9226ad0
Kirill Voloshin <tehpolecat@gmail.com>
2025-09-25 20:28:50 +0200

    ks: some autocmds

---
+++ nvim-kickstart/lua/custom/config/autocmds.lua
@@ -0,0 +1,23 @@
+local function augroup(name)
+	return vim.api.nvim_create_augroup(name, { clear = true })
+end
+
+vim.api.nvim_create_autocmd("TextYankPost", {
+	desc = "Highlight when yanking (copying) text",
+	group = augroup("highlight_yank"),
+	callback = function()
+		vim.hl.on_yank()
+	end,
+})
+
+-- Auto create dir when saving a file, in case some intermediate directory does not exist
+vim.api.nvim_create_autocmd({ "BufWritePre" }, {
+	group = augroup("auto_create_dir"),
+	callback = function(event)
+		if event.match:match("^%w%w+:[\\/][\\/]") then
+			return
+		end
+		local file = vim.uv.fs_realpath(event.match) or event.match
+		vim.fn.mkdir(vim.fn.fnamemodify(file, ":p:h"), "p")
+	end,
+})
