af599b23a38361e631cc4c23856909bf322c6706
Kirill Voloshin <tehpolecat@gmail.com>
2025-09-26 20:44:13 +0200

    ks: remove redundant keymaps

--- nvim-kickstart/lua/custom/config/keymaps.lua
+++ nvim-kickstart/lua/custom/config/keymaps.lua
@@ -1,24 +1,5 @@
 local map = vim.keymap.set
 
--- lazygit
-if vim.fn.executable("lazygit") == 1 then
-	map("n", "<leader>gg", function()
-		Snacks.lazygit()
-	end, { desc = "Lazygit (Root Dir)" })
-	map("n", "<leader>gG", function()
-		Snacks.lazygit()
-	end, { desc = "Lazygit (cwd)" })
-	map("n", "<leader>gf", function()
-		Snacks.picker.git_log_file()
-	end, { desc = "Git Current File History" })
-	map("n", "<leader>gl", function()
-		Snacks.picker.git_log({ cwd = LazyVim.root.git() })
-	end, { desc = "Git Log" })
-	map("n", "<leader>gL", function()
-		Snacks.picker.git_log()
-	end, { desc = "Git Log (cwd)" })
-end
-
 -- Save File
 map({ "i", "x", "n", "s" }, "<C-s>", "<cmd>w<cr><esc>", { desc = "Save File" })
 
