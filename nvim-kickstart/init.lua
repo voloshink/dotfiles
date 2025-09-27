62a0eb72203ff785d7ff7b9408cd895b86f0dc80
Kirill Voloshin <tehpolecat@gmail.com>
2025-09-27 12:23:36 +0200

    ks: todo-comments

--- nvim-kickstart/init.lua
+++ nvim-kickstart/init.lua
@@ -572,14 +572,6 @@ require("lazy").setup({
 			signature = { enabled = true },
 		},
 	},
-	-- Highlight todo, notes, etc in comments
-	{
-		"folke/todo-comments.nvim",
-		event = "VimEnter",
-		dependencies = { "nvim-lua/plenary.nvim" },
-		opts = { signs = false },
-	},
-
 	{ -- Highlight, edit, and navigate code
 		"nvim-treesitter/nvim-treesitter",
 		build = ":TSUpdate",
