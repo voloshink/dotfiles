a18818dc73f667d43ba405dfe01b89be823e0b3c
Kirill Voloshin <tehpolecat@gmail.com>
2025-10-18 00:13:01 +0200

    ks: update mini.nvim

--- nvim-kickstart/lua/custom/plugins/mini.lua
+++ nvim-kickstart/lua/custom/plugins/mini.lua
@@ -1,31 +1,44 @@
 return {
-	"echasnovski/mini.nvim",
-	-- Better Around/Inside textobjects
-	--
-	-- Examples:
-	--  - va)  - [V]isually select [A]round [)]paren
-	--  - yinq - [Y]ank [I]nside [N]ext [Q]uote
-	--  - ci'  - [C]hange [I]nside [']quote
-	require("mini.ai").setup({ n_lines = 500 }),
+	{
+		-- Better Around/Inside textobjects
+		--
+		-- Examples:
+		--  - va)  - [V]isually select [A]round [)]paren
+		--  - yinq - [Y]ank [I]nside [N]ext [Q]uote
+		--  - ci'  - [C]hange [I]nside [']quote
+		"nvim-mini/mini.ai",
+		version = "*",
+		opts = {
+			n_lines = 500,
+		},
+	},
 
-	-- Add/delete/replace surroundings (brackets, quotes, etc.)
-	--
-	-- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
-	-- - sd'   - [S]urround [D]elete [']quotes
-	-- - sr)'  - [S]urround [R]eplace [)] [']
-	require("mini.surround").setup(),
+	{
 
-	-- Auto pair
-	require("mini.pairs").setup({
-		modes = { insert = true, command = true, terminal = false },
-		-- skip autopair when next character is one of these
-		skip_next = [=[[%w%%%'%[%"%.%`%$]]=],
-		-- skip autopair when the cursor is inside these treesitter nodes
-		skip_ts = { "string" },
-		-- skip autopair when next character is closing pair
-		-- and there are more closing pairs than opening pairs
-		skip_unbalanced = true,
-		-- better deal with markdown code blocks
-		markdown = true,
-	}),
+		-- Add/delete/replace surroundings (brackets, quotes, etc.)
+		--
+		-- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
+		-- - sd'   - [S]urround [D]elete [']quotes
+		-- - sr)'  - [S]urround [R]eplace [)] [']
+		"nvim-mini/mini.surround",
+		version = "*",
+	},
+	{
+		-- Auto pair
+		"nvim-mini/mini.pairs",
+		version = "*",
+		opts = {
+
+			modes = { insert = true, command = true, terminal = false },
+			-- skip autopair when next character is one of these
+			skip_next = [=[[%w%%%'%[%"%.%`%$]]=],
+			-- skip autopair when the cursor is inside these treesitter nodes
+			skip_ts = { "string" },
+			-- skip autopair when next character is closing pair
+			-- and there are more closing pairs than opening pairs
+			skip_unbalanced = true,
+			-- better deal with markdown code blocks
+			markdown = true,
+		},
+	},
 }
