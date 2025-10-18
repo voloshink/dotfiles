9d69c01b762b6d80a9f13de5e8caf4404182d452
Kirill Voloshin <tehpolecat@gmail.com>
2025-10-18 19:56:42 +0200

    ks: keymap for terminal

--- nvim-kickstart/lua/custom/config/keymaps.lua
+++ nvim-kickstart/lua/custom/config/keymaps.lua
@@ -49,3 +49,7 @@ map("n", "]e", diagnostic_goto(true, "ERROR"), { desc = "Next Error" })
 map("n", "[e", diagnostic_goto(false, "ERROR"), { desc = "Prev Error" })
 map("n", "]w", diagnostic_goto(true, "WARN"), { desc = "Next Warning" })
 map("n", "[w", diagnostic_goto(false, "WARN"), { desc = "Prev Warning" })
+
+-- terminal
+map({ "n", "t" }, "<c-/>", function() Snacks.terminal() end,
+	{ desc = "Terminal (Root Dir)" })
