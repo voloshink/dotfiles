b243e5b52008e83afdabab68919f56a8097bacdf
Kirill Voloshin <tehpolecat@gmail.com>
2025-10-18 19:59:04 +0200

    ks: init cleanup

--- nvim-kickstart/init.lua
+++ nvim-kickstart/init.lua
@@ -106,45 +106,8 @@ require("lazy").setup({
   { import = "custom.plugins" },
   "NMAC427/guess-indent.nvim", -- Detect tabstop and shiftwidth automatically
 
-  -- NOTE: Plugins can also be configured to run Lua code when they are loaded.
-  --
-  -- This is often very useful to both group configuration, as well as handle
-  -- lazy loading plugins that don't need to be loaded immediately at startup.
-  --
-  -- For example, in the following configuration, we use:
-  --  event = 'VimEnter'
-  --
-  -- which loads which-key before all the UI elements are loaded. Events can be
-  -- normal autocommands events (`:help autocmd-events`).
-  --
-  -- Then, because we use the `opts` key (recommended), the configuration runs
-  -- after the plugin has been loaded as `require(MODULE).setup(opts)`.
-
-  -- NOTE: Plugins can specify dependencies.
-  --
-  -- The dependencies are proper plugin specifications as well - anything
-  -- you do for a plugin at the top level, you can do for a dependency.
-  --
-  -- Use the `dependencies` key to specify the dependencies of a particular plugin
-
-
-  -- 			lua_ls = {
-  -- 				-- cmd = { ... },
-  -- 				-- filetypes = { ... },
-  -- 				-- capabilities = {},
-  -- 				settings = {
-  -- 					Lua = {
-  -- 						completion = {
-  -- 							callSnippet = "Replace",
-  -- 						},
-  -- 						-- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
-  -- 						-- diagnostics = { disable = { 'missing-fields' } },
-  -- 					},
-  -- 				},
-  -- 			},
-  -- 		}
-
-  { -- Autoformat
+  {
+    -- Autoformat
     "stevearc/conform.nvim",
     event = { "BufWritePre" },
     cmd = { "ConformInfo" },
