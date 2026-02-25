afedc184f46bee9f8451d1ccc8f913af5f5f0084
Kirill Voloshin <kirill.voloshin@teampicnic.com>
2026-02-25 11:17:00 +0100

    Disable conceallevel for JSON files

--- nvim/lua/custom/config/autocmds.lua
+++ nvim/lua/custom/config/autocmds.lua
@@ -10,6 +10,19 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 	end,
 })
 
+-- conceallevel is a window-local option, so when markview.nvim sets it to 3
+-- for markdown rendering, it persists when switching to a different buffer in
+-- the same window. nvim-treesitter ships JSON highlight queries that conceal
+-- quote characters when conceallevel > 0, causing string quotes to disappear
+-- in normal mode. Explicitly resetting it for JSON files prevents this.
+vim.api.nvim_create_autocmd("FileType", {
+	group = augroup("json_no_conceal"),
+	pattern = { "json", "jsonc" },
+	callback = function()
+		vim.opt_local.conceallevel = 0
+	end,
+})
+
 -- Auto create dir when saving a file, in case some intermediate directory does not exist
 vim.api.nvim_create_autocmd({ "BufWritePre" }, {
 	group = augroup("auto_create_dir"),
